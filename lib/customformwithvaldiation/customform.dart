77fd269d70da8c99ce9663d48f6110e9a29f3adb
AkshaySarkale <akshaysarkale999@gmail.com>
2026-06-01 11:44:05 +0530

    subtitle

--- lib/customformwithvaldiation/customform.dart
+++ lib/customformwithvaldiation/customform.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+
 class Customform extends StatefulWidget {
-  final String  pageTitle;
-  final String? subTitle;
-  const Customform({super.key,
-    required this.pageTitle,
-    this.subTitle});
+  final String pageTitle;
+  final String subTitle;
+
+  const Customform({super.key, required this.pageTitle, this.subTitle=""});
 
   @override
   State<Customform> createState() => _CustomformState();
@@ -13,13 +13,15 @@ class Customform extends StatefulWidget {
 class _CustomformState extends State<Customform> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text(widget.pageTitle),
-      ],
-    )
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(widget.pageTitle),
+          Text(widget.subTitle),
+        ],
+      ),
     );
   }
 }
