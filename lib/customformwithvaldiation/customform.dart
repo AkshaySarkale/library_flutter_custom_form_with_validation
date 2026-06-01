ae908ef29ba1324d5bb569c253cb349e807446b0
AkshaySarkale <akshaysarkale999@gmail.com>
2026-06-01 10:39:45 +0530

    inizilize commit

---
+++ lib/customformwithvaldiation/customform.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Customform extends StatefulWidget {
+  const Customform({super.key});
+
+  @override
+  State<Customform> createState() => _CustomformState();
+}
+
+class _CustomformState extends State<Customform> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
